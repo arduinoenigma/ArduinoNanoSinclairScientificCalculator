899da2500a88bced679a06c6abc3e238762c7b12
Arduino Enigma <arduinoenigma@gmail.com>
2018-01-01 19:52:33 -0500

    Update SinclairScientific1.ino

--- SinclairScientific1/SinclairScientific1.ino
+++ SinclairScientific1/SinclairScientific1.ino
@@ -111,27 +111,30 @@ const unsigned long LISTOPSWITHK = 1007135334;
 //this.fastStep = 1; // 1 makes SYNC, etc take 1 cycle
 //this.power = 1; // On
 
+
 struct SinclairData_t
 {
+  boolean sinclair = true;
+
   // Important: Array order matches display order, not bit order.
   // I.e. a[0] is high-order digit S10, a[10] is low-order digit S0.
-  unsigned int a = 0;     // Register A
-  unsigned int b = 0;     // Register B
-  unsigned int c = 0;     // Register C
-  unsigned int af = 0;    // Flags A
-  unsigned int bf = 0;    // Flags B
+  signed char  a[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};    // Register A
+  signed char  b[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};    // Register B
+  signed char  c[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};    // Register C
+  signed char af[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};    // Flags A
+  signed char bf[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};    // Flags A
 
-  unsigned int d = 1023;  // D scan register, d[0] low for D1
+  signed char  d[11] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
   // Currently active D value 1-11. d[dActive-1] == 0
   byte dActive = 1;
-  byte cc = 0;
+  signed char cc = 0;
   char ccMeaning[12];
 
   byte keyPressed = 0;
   // 'KO' or 'KP' if a keyboard input line is active, i.e. dActive and keyPressed match in the key matrix
   byte keyStrobe = 0;
-  byte address = 0; // PROGRAM COUNTER
+  unsigned int address = 0; // PROGRAM COUNTER
   byte display = 1; // Flag for display on
   char mask[MASK_LENGTH];
   byte showDisplayScan = 0;
@@ -141,23 +144,406 @@ struct SinclairData_t
   byte power = 1; // On
 } SinclairData;
 
+boolean opsWithK(byte opcode) {
+  return (LISTOPSWITHK & (1UL << opcode));
+}
 
 unsigned int getInstruction(unsigned int PC) {
+
   return pgm_read_word_near(objectCode + PC);
 }
 
-void getMaskNum() {
-  //return this.model.rom[this.model.address] & 0xf;
+byte getMaskNum() {
+
   return getInstruction(SinclairData.address) & 0x0f;
 }
 
 void getMask() {
 
+  unsigned int instruction = getInstruction(SinclairData.address);
+  byte classBits = instruction >> 9;
+  byte opcode = (instruction >> 4) & 0x1f;
+
+  if (classBits == 3 || (classBits == 2 && opcode > 18 && opcode != 21 && opcode != 22))
+  {
+    byte maskno = getMaskNum();
+
+    for (byte i = 0; i <= 10; i++)
+    {
+      char maskdigit = pgm_read_byte_near(masks[maskno] + i);
+
+      if (maskdigit == ' ')
+      {
+        SinclairData.mask[i] = maskdigit;
+      }
+      else if (classBits == 3 && opsWithK(opcode))
+      {
+        // Register instruction
+        SinclairData.mask[i] = maskdigit;
+      }
+      else
+      {
+        SinclairData.mask[i] = '*';
+      }
+    }
+  }
+  else
+  {
+    SinclairData.mask[0] = 0;
+  }
 }
 
-boolean opsWithK(byte opcode) {
-  return (LISTOPSWITHK & (1UL << opcode));
+
+void step() {
+
+  Serial.println(F("step()"));
+
+  unsigned int instruction = getInstruction(SinclairData.address);
+  byte classBits = instruction >> 9;
+  byte opcode = (instruction >> 4) & 0x1f;
+  unsigned int nextAddress = SinclairData.address + 1;
+
+  if (classBits == 3)
+  {
+    // Register instruction
+
+    byte maskBits = instruction & 0xf;
+    switch (opcode)
+    {
+      case 0: // AABA: A+B -> A
+        this.add(this.model.a, this.model.b, this.model.a);
+        break;
+      case 1: // AAKA: A+K -> A
+        this.add(this.model.a, this.getMask(), this.model.a);
+        break;
+      case 2: // AAKC: A+K -> C
+        this.add(this.model.a, this.getMask(), this.model.c);
+        break;
+      case 3:
+        if (sinclair)
+        { // ACBB C+B -> B
+          this.add(this.model.c, this.model.b, this.model.b);
+        }
+        else
+        { // ABOA: B -> A
+          this.copy(this.model.b, this.model.a);
+        }
+        break;
+      case 4: // ABOC: B -> C
+        this.copy(this.model.b, this.model.c);
+        break;
+      case 5: // ACKA: C+K -> A
+        this.add(this.model.c, this.getMask(), this.model.a);
+        break;
+      case 6: // AKCB: C+K -> B
+        this.add(this.model.c, this.getMask(), this.model.b);
+        break;
+      case 7: // SABA: A-B -> A
+        this.sub(this.model.a, this.model.b, this.model.a);
+        break;
+      case 8: // SABC: A-B -> C
+        this.sub(this.model.a, this.model.b, this.model.c);
+        break;
+      case 9: // SAKA: A-K -> A
+        this.sub(this.model.a, this.getMask(), this.model.a);
+        break;
+      case 10: // SCBC: C-B -> C
+        this.sub(this.model.c, this.model.b, this.model.c);
+        break;
+      case 11: // SCKC: C-K -> C
+        this.sub(this.model.c, this.getMask(), this.model.c);
+        break;
+      case 12: // CAB: compare A-B
+        this.compare(this.model.a, this.model.b);
+        break;
+      case 13: // CAK: compare A-K
+        this.compare(this.model.a, this.getMask());
+        break;
+      case 14: // CCB: compare C-B
+        this.compare(this.model.c, this.model.b);
+        break;
+      case 15: // CCK: compare C-K
+        this.compare(this.model.c, this.getMask());
+        break;
+      case 16: // AKA: K -> A
+        this.copy(this.getMask(), this.model.a);
+        break;
+      case 17: // AKB: K -> B
+        this.copy(this.getMask(), this.model.b);
+        break;
+      case 18: // AKC: K -> C
+        this.copy(this.getMask(), this.model.c);
+        break;
+      case 19: // EXAB: exchange A and B
+        this.exchange(this.model.a, this.model.b);
+        break;
+      case 20: // SLLA: shift A left
+        this.sll(this.model.a);
+        break;
+      case 21: // SLLB: shift B left
+        this.sll(this.model.b);
+        break;
+      case 22: // SLLC: shift C left
+        this.sll(this.model.c);
+        break;
+      case 23: // SRLA: shift A right
+        this.srl(this.model.a);
+        break;
+      case 24: // SRLB: shift B right
+        this.srl(this.model.b);
+        break;
+      case 25: // SRLC: shift C right
+        this.srl(this.model.c);
+        break;
+      case 26: // AKCN: A+K -> A until key down on N or D11 [sic]
+        // Patent says sets condition if key down, but real behavior
+        // is to set condition if addition overflows (i.e. no key down)
+        this.add(this.model.a, this.getMask(), this.model.a);
+        if (this.model.keyStrobe == 'KN')
+        {
+          // Advance to next instruction
+        }
+        else if (this.model.dActive != 10)
+        {
+          // Hold at current instruction and continue scan
+          nextAddress = this.model.address;
+        }
+        else
+        {
+          // For state d10, fall through
+        }
+        break;
+      case 27:
+        if (sinclair)
+        { // SCBA C-B -> A
+          this.sub(this.model.c, this.model.b, this.model.a);
+        }
+        else
+        { // AAKAH A+K -> A hex
+          this.add(this.model.a, this.getMask(), this.model.a, 1 /* hex */ );
+          this.model.cc = 0;
+          this.model.ccMeaning = '';
+        }
+        break;
+      case 28:
+        if (sinclair)
+        { // SCKB C-K -> B
+          this.sub(this.model.c, this.getMask(), this.model.b);
+        }
+        else
+        { // SAKAH A-K -> A hex
+          this.sub(this.model.a, this.getMask(), this.model.a, 1 /* hex */ );
+          this.model.cc = 0;
+          this.model.ccMeaning = '';
+        }
+        break;
+      case 29: // ACKC: C+K -> C
+        this.add(this.model.c, this.getMask(), this.model.c);
+        break;
+      case 30:
+        if (sinclair)
+        { // AABC A+B -> C
+          this.add(this.model.a, this.model.b, this.model.c);
+          break;
+        }
+      case 31:
+        if (sinclair)
+        { // ACBC C+B -> C
+          this.add(this.model.c, this.model.b, this.model.c);
+          break;
+        }
+      default:
+        alert('Bad instruction ' + instruction);
+        break;
+    }
+  }
+  else if ((instruction >> 8) == 5)
+  {
+    // Flag instruction
+    var maskBits = instruction & 0xf;
+    switch (opcode)
+    {
+      case 16: // NOP
+        break;
+      case 17: // WAITDK: wait for display key
+        this.model.display = 0;
+        if (this.model.keyPressed == 'DK')
+        {
+          // Jump
+          nextAddress = instruction & 0x1ff;
+        }
+        else
+        {
+          // Hold address until DK pressed
+          nextAddress = this.model.address;
+        }
+        break;
+      case 18: // WAITNO: wait for key or address register overflow
+        if (this.model.keyStrobe)
+        {
+          // Jump
+          nextAddress = instruction & 0x1ff;
+        }
+        else
+        {
+          // Hold address until key pressed or address overflow (TODO)
+          nextAddress = this.model.address;
+        }
+        break;
+      case 19: // SFB: set flag B
+        this.writeFlag(this.model.bf, 1);
+        break;
+      case 20: // SFA: set flag A
+        this.writeFlag(this.model.af, 1);
+        break;
+      case 21: // SYNC (SYNCH): hold address until end of D10
+        if (this.model.dActive != 10)
+        {
+          nextAddress = this.model.address;
+        }
+        this.model.cc = 0;
+        this.model.ccMeaning = '';
+        break;
+      case 22: // SCAN (SCANNO): wait for key
+        this.model.display = 1; // Reset display power off latch
+        if (this.model.keyStrobe)
+        {
+          this.model.cc = 1;
+          this.model.ccMeaning = 'key';
+        }
+        else
+        {
+          this.model.cc = 0;
+          this.model.ccMeaning = 'no key';
+          if (this.model.dActive != 10)
+          {
+            // Hold address until end of D10
+            nextAddress = this.model.address;
+          }
+        }
+        break;
+      case 23: // ZFB: zero flag B
+        this.writeFlag(this.model.bf, 0);
+        break;
+      case 24: // ZFA: zero flag A
+        this.writeFlag(this.model.af, 0);
+        break;
+      case 25: // TFB: test flag B
+        this.testFlag(this.model.bf);
+        break;
+      case 26: // TFA: test flag A
+        this.testFlag(this.model.af);
+        break;
+      case 27: // FFB: flip flag B
+        this.writeFlag(this.model.bf, -1 /* flip */ );
+        break;
+      case 28: // FFA: flip flag A
+        this.writeFlag(this.model.af, -1 /* flip */ );
+        break;
+      case 29: // CF: compare flags
+        this.compareFlags(this.model.af, this.model.bf);
+        break;
+      case 30: // NOP
+        break;
+      case 31: // EXF: exchange flags
+        this.exchange(this.model.af, this.model.bf);
+        break;
+      default:
+        alert('Bad instruction ' + instruction);
+        break;
+    }
+  }
+  else if (classBits == 0)
+  {
+    // jump if reset: BIU, BIZ, BIGE, BINC, BIE, BET
+    if (this.model.cc == 0)
+    {
+      nextAddress = instruction & 0x1ff;
+    }
+    this.model.cc = 0; // Clear after jump
+    this.model.ccMeaning = '';
+  }
+  else if (classBits == 1)
+  {
+    // jump if set: BID, BIO, BILT, BIC, BINE
+    if (this.model.cc == 1)
+    {
+      nextAddress = instruction & 0x1ff;
+    }
+    this.model.cc = 0; // Clear after jump
+    this.model.ccMeaning = '';
+  }
+  else if ((instruction >> 7) == 8)
+  {
+    // Jump if key down on KO (BKO)
+    if (this.model.keyStrobe == 'KO')
+    {
+      nextAddress = instruction & 0x1ff;
+    }
+    this.model.cc = 0; // Clear after jump
+    this.model.ccMeaning = '';
+  }
+  else if ((instruction >> 7) == 9)
+  {
+    // Jump if key down on KP (BKP)
+    if (this.model.keyStrobe == 'KP')
+    {
+      nextAddress = instruction & 0x1ff;
+    }
+    this.model.cc = 0; // Clear after jump
+    this.model.ccMeaning = '';
+  }
+  else
+  {
+    alert('Bad instruction code ' + instruction);
+  }
+  this.model.address = nextAddress;
+  // Put the mask for the next instruction in the model for display
+  this.model.mask = this.getMask();
+  // Update D state
+  this.updateD();
 }
+
+
+add (src1, src2, dst, hex)
+{
+  var carry = 0;
+  var maskVec = this.getMask();
+  for (var i = 10; i >= 0; i--)
+  {
+    if (maskVec[i] == = ' ')
+    {
+      // masked out
+      continue;
+    }
+    else
+    {
+      var result = src1[i] + src2[i] + carry;
+      if (!hex && result >= 10)
+      {
+        result -= 10;
+        carry = 1;
+      }
+      else if (hex && result >= 16)
+      {
+        result -= 16;
+        carry = 1;
+      }
+      else
+      {
+        carry = 0;
+      }
+      dst[i] = result;
+    }
+  }
+  if (carry)
+  {
+    this.model.cc = carry;
+    this.model.ccMeaning = carry ? 'overflow' : 'no overflow';
+  }
+};
+
+
 
 
 void setup() {
@@ -165,9 +551,15 @@ void setup() {
 
   Serial.begin(9600);
 
-  test_ListAllOpsWithK();
-  test_ListProgram();
-  test_DisplaySinclairData();
+  /*
+    test_setBit();
+    test_getBit();
+    test_ListAllOpsWithK();
+    test_ListProgram();
+    test_DisplaySinclairData();
+  */
+
+  step();
 
 }
 
