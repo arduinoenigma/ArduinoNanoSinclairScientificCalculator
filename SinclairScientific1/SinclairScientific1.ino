35a76b5ce5e748b3e93486d47cbf6c443a832c0a
Arduino Enigma <arduinoenigma@gmail.com>
2018-01-15 00:36:52 -0500

    calibrated delay for accuracy. cleaned code.

--- SinclairScientific1/SinclairScientific1.ino
+++ SinclairScientific1/SinclairScientific1.ino
@@ -4,15 +4,9 @@
 // Ken Shirriff, http://righto.com/ti
 // Based on patent US3934233
 //
-//
 // The goal of this project is to run the following simulator: http://righto.com/sinclair
 // on an arduino nano powered custom pcb resembling the original Sinclair Scientific Calculator
 // @arduinoenigma 2018
-
-// TODO:
-// match PrintableKeys to
-// SinclairData.keyStrobe  -> activate KN KO KP
-// SinclairData.keyPressed -> activate DK
 
 #include "GPIO.h"
 
@@ -23,9 +17,9 @@ extern void allSegmentOff();
 extern void allDigitOutput();
 extern void allDigitOff();
 extern void step();
-extern char readKey();
+extern byte readKey();
 
-unsigned int const objectCode[] PROGMEM = {
+const unsigned int objectCode[] PROGMEM = {
   1408, 1392, 1792, 1824, 1860, 1808, 1360, 1376,
   518, 1319, 1360, 1376, 9, 1360, 1908, 1072,
   1083, 1075, 1121, 1129, 1073, 1069, 1051, 1840,
@@ -90,31 +84,31 @@ const char masks [][MASK_LENGTH] PROGMEM = {
   {"    0      "}, // M15
 };
 
-//OPS WITH K: 1:AAKA 2:AAKC 5:ACKA 6:ACKB 9: 11:
+// OPS WITH K: 1:AAKA 2:AAKC 5:ACKA 6:ACKB 9: 11:
 //const unsigned long LISTOPSWITHK = 000111100000001111010101001100110b;
 const unsigned long LISTOPSWITHK = 1007135334;
 
+char keyindex = 0;
 char key = 0;
-char p = 0;
 boolean resetRequested = false;
 boolean displayCleared = false;
 
 signed char digits[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-char PrintableKeys[19] = "12+E0v-378X654/9^C";
+const char PrintableKeys[19] = "12+E0v-378X654/9^C";
 
 const char keysKN[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 0};
 const char keysKO[10] = {'C', 'v', '+', '-', '/', 'X', '^', 'E', '0', 0};
 //const char keysKP[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-//to use with keyStrobe
+// to use with keyStrobe
 #define KN 1
 #define KO 2
 #define KP 3
 
-//to use with keyPressed
+// to use with keyPressed
 #define DK 1
 
-//ORIGINAL SINCLAIR DATA
+// ORIGINAL SINCLAIR DATA
 struct SinclairData_t
 {
   boolean sinclair = true;
@@ -132,36 +126,31 @@ struct SinclairData_t
   // Currently active D value 1-11. d[dActive-1] == 0
   byte dActive = 1;
   signed char cc = 0;
-  char ccMeaning[12];
-
   byte keyPressed = 0;
   // 'KO' or 'KP' if a keyboard input line is active, i.e. dActive and keyPressed match in the key matrix
   byte keyStrobe = 0;
   unsigned int address = 0; // PROGRAM COUNTER
   byte display = 1; // Flag for display on
   char mask[MASK_LENGTH];
-  byte showDisplayScan = 0;
-  byte idle = 0; // 1 if in idle loop
-  byte speed = 0; // Speed for updates
-  byte fastStep = 1; // 1 makes SYNC, etc take 1 cycle
-  byte power = 1; // On
 } SinclairData;
 
-//this function is called by the display routines to show a digit for 2ms before moving to the next one.
-//can do useful work here... instead of delay(2);
+// this function is called by the display routines to show a digit for 2ms before moving to the next one.
+// can do useful work here... instead of delay();
+// Sin 1 takes about 7.5 seconds.
+// Arccos .2 takes about 15 seconds
+// Arccos of a very small value (e.g. 0.0005) goes into an almost-infinite loop and takes 1 minute, 48 seconds to complete
+
 void backgroundWork() {
 
+  delayMicroseconds(142);
   stepAndScan();
-  stepAndScan();
-  stepAndScan();
-  stepAndScan();
-  stepAndScan();
-
+  //stepAndScan(); //any more makes it too fast
+  delayMicroseconds(142);
 }
 
 void updateDisplay() {
 
-  digits[0] = (SinclairData.a[0] == 0) ? 99 : 10;;
+  digits[0] = (SinclairData.a[0] == 0) ? 99 : 10;
   digits[1] = SinclairData.a[4];
   digits[2] = SinclairData.a[5];
   digits[3] = SinclairData.a[6];
@@ -171,25 +160,13 @@ void updateDisplay() {
   digits[7] = SinclairData.a[2];
   digits[8] = SinclairData.a[3];
 
-  /*
-    digits[0] = 0;
-    digits[1] = 0;
-    digits[2] = (SinclairData.a[0] == 0) ? 99 : 10;
-    digits[3] = SinclairData.a[4];
-    digits[4] = SinclairData.a[5];
-    digits[5] = SinclairData.a[6]; //7 and 8 also when we get digits 0 and 1 working
-    digits[6] = (SinclairData.a[1] == 0) ? 99 : 10;
-    digits[7] = SinclairData.a[2];
-    digits[8] = SinclairData.a[3];
-  */
-
   display();
 }
 
 void setup() {
   // put your setup code here, to run once:
 
-  //Serial.begin(250000);
+  //Serial.begin(250000); // Cannot even call this function if displays are enabled. conflicts with use of D0 and D1
 
   allKeyRowOff();
 
@@ -198,13 +175,12 @@ void setup() {
 
   allDigitOutput();
   allDigitOff();
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  key = readKey();
+  keyindex = readKey();
 
   stepAndScan();
 
@@ -227,40 +203,37 @@ void loop() {
       }
     }
   }
-
 }
 
 void stepAndScan() {
 
   SinclairData.keyStrobe = 0;
-  p = 0;
+  key = 0;
 
-  if (key != 0)
+  if (keyindex != 0)
   {
-    p = PrintableKeys[key - 1];
-    //Serial.println(PrintableKeys[key - 1]);
+    key = PrintableKeys[keyindex - 1];
 
-    if (p == keysKN[SinclairData.dActive - 1]) {
+    if (key == keysKN[SinclairData.dActive - 1]) {
       SinclairData.keyStrobe = KN;
     }
 
-    if (p == keysKO[SinclairData.dActive - 1]) {
+    if (key == keysKO[SinclairData.dActive - 1]) {
       SinclairData.keyStrobe = KO;
     }
 
-    if (p == 'C') {
+    if (key == 'C') {
       resetRequested = true;
     }
   }
 
-  if (!resetRequested)
+  if (resetRequested)
+  {
+    delayMicroseconds(50);
+  }
+  else
   {
     step();
     displayCleared = false;
   }
-  else
-  {
-    delayMicroseconds(500);
-  }
-
 }
